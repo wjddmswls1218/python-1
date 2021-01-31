14481fcbb9d1ea1c07e6cd11c6ea64eee46e7620
User <JEJ>
2021-01-31 14:25:27 +0900

    py

--- init_game.py
+++ init_game.py
@@ -1,33 +1,34 @@
 import pygame
 
-pygame.init()
+pygame.init()  # 초기화 - 반드시 필요
 
 # 화면 크기 설정
 screen_width = 640
-screen_height = 860
+screen_heigth = 860
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_heigth))
 
-# game title
-pygame.display.set_caption("BASIC GAME")
+# 화면 타이틀 설정
+pygame.display.set_caption("BAISC GAME")
 
-
-# 배경 이미지 조정
+# 배경이미지 설정
 background = pygame.image.load(
-    "/Users/jyr/Coding/4leaf-education/PYTHONN_GAME/basic_game/background.png"
+    "/Users/user/Documents/python_game/background.png"
 )
 
+# 케릭터 불러오기
 character = pygame.image.load(
-    "/Users/jyr/Coding/4leaf-education/PYTHONN_GAME/basic_game/character.png"
+    "/Users/user/Documents/python_game/character.png"
+
 )
 
-char_size = character.get_rect().size
+char_size = character.get_rect().size  # 이미지 크기를 구해온다.
 
 char_width = char_size[0]
-char_height = char_size[1]
+char_heigth = char_size[1]
 
 char_position_x = (screen_width / 2) - (char_width / 2)
-char_position_y = screen_height - char_height
+char_position_y = screen_heigth - char_heigth
 
 # move
 to_x = 0
@@ -60,16 +61,16 @@ while running:
     char_position_x += to_x
     char_position_y += to_y
 
-    if char_position_x <= 0:
+    if char_position_x < 0:
         char_position_x = 0
-    elif char_position_x >= screen_width-char_width:
+    elif char_position_x > screen_width-char_width:
         char_position_x = screen_width - char_width
-    elif char_position_y <= 0:
+    elif char_position_y < 0:
         char_position_y = 0
-    elif char_position_y >= screen_height - char_height:
-        char_position_y = screen_height - char_height
+    elif char_position_y > screen_heigth - char_heigth:
+        char_position_y = screen_heigth - char_heigth
 
-    screen.blit(background, (char_position_x, char_position_y))
+    screen.blit(background, (0, 0))
     screen.blit(character, (char_position_x, char_position_y))
 
-    pygame.display.update()
+    pygame.display.update()  # 화면 다시 그리기
